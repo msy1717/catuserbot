604bc915c0a48735a526a65c8d015b463f4ea356
msy1717 <80758264+msy1717@users.noreply.github.com>
2021-09-21 13:43:46 +0530

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -18,13 +18,28 @@ async def ex(event):
                     ])
 @BotzHub.on(events.callbackquery.CallbackQuery(data="lgi"))
 async def ex(event):
-    await event.edit("**Commands For logo**\n🔹 `/logo <text>`\n🔹`/wlogo`<text>")
-    button = InlineKeyboardMarkup().add(
-        InlineKeyboardButton(text="🏃‍♂️ Back", callback_data="semx")
-    )
+    await event.edit("**Commands For logo**\n🔹 `/logo <text>`\n🔹`/wlogo`<text>",parse_mode=ParseMode.MARKDOWN,
+            disable_web_page_preview=True,#when needed
+            reply_markup=InlineKeyboardMarkup(
+                [
+                 [
+                    InlineKeyboardButton(text="Back", callback_data="semx")
+                 ]
+                ]
+            ),
+        )
+    
 @BotzHub.on(events.callbackquery.CallbackQuery(data="op"))
 async def ex(event):
-    await event.edit("**Commands For ipfinder**\n🔹 `/ip <iphere>`")
-    button = InlineKeyboardMarkup().add(
-        InlineKeyboardButton(text="🏃‍♂️ Back", callback_data="semx")
-    )
+    await event.edit("**Commands For ipfinder**\n🔹 `/ip <iphere>`",parse_mode=ParseMode.MARKDOWN,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                 [
+                    InlineKeyboardButton(text="Back", callback_data="semx")
+                 ]
+                ]
+            ),
+        )
+    
+        
