52e39b0bbe18c3094a2e8606a4cebf0f10ce7ed1
msy1717 <80758264+msy1717@users.noreply.github.com>
2021-09-21 15:17:28 +0530

    Update ip.py

--- TelethonBot/plugins/ip.py
+++ TelethonBot/plugins/ip.py
@@ -1,1 +1,10 @@
-
+CMD_HELP.update(
+    {
+        "time": ">`.time <country name/code> <timezone number>`"
+        "\nUsage: Get the time of a country. If a country has "
+        "multiple timezones, it will list all of them and let you select one.",
+        "date": ">`.date <country name/code> <timezone number>`"
+        "\nUsage: Get the date of a country. If a country has "
+        "multiple timezones, it will list all of them and let you select one.",
+    }
+)
